badb1c954042a068624815349f42f9b466c02ba1
Hemanathan5555 <cbihema005@gmail.com>
2025-01-21 21:17:15 +0530

    Add files via upload

---
+++ python/strings repeated.py
@@ -0,0 +1,13 @@
+a="abcd"
+c=set(a)#removing the repeating terms
+count=0
+for i in range(0,len(c)):
+    count=count+1#it will get increment depending upon length of c
+print(count)
+
+a="abab"
+c=set(a)#removing the repeating terms
+count=0
+for i in range(0,len(c)):
+    count=count+1#it will get increment depending upon length of c
+print(count)
