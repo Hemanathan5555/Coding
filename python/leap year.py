8054549ca57ca77c8bb395c82719193b95856a1b
Hemanathan5555 <cbihema005@gmail.com>
2025-01-29 21:39:39 +0530

    Add files via upload

---
+++ python/leap year.py
@@ -0,0 +1,6 @@
+a=1108
+if (a%4==0) and (a%100!=0 or a%400==0):#if a is divide by 4 and 400 equal to 0 means its a leap yr
+    #or a is divided by 4 and  divided by 100 not equal to 0 means its leap yr
+    print("its leap year")
+else:
+    print("no leap year")
