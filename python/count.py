47c2af55b0e76b5c6292c1b5359203a7128e5760
Hemanathan5555 <cbihema005@gmail.com>
2025-01-30 21:15:24 +0530

    Update count.py

--- python/count.py
+++ python/count.py
@@ -1,9 +1,9 @@
 a=[1,1,2,3,3]
-s=set(a)
+s=set(a)#removing duplicates
 count=0
 for i in s:
     k=a.count(i)
-    if k>1:
+    if k>1:#if count of an interger is more than 1
         count=count+1
     else:
         pass
